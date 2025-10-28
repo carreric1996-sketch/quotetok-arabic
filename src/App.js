1f86e3002f1c2721e22e86bf734bf9afb86a0e44
carreric1996-sketch <carr.eric.1996@gmail.com>
2025-10-28 22:10:12 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+function App() { return <div className="text-center p-10">QuoteTok Arabic Starter</div>; }
+export default App;
---
+++ src/supabaseClient.js
@@ -0,0 +1,2 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient('https://YOUR_PROJECT.supabase.co','YOUR_ANON_KEY');
